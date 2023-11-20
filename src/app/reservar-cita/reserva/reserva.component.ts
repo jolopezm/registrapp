4b42084a7d703aa5776fe0e9f0412708066b0541
jolopezm <joselopezmignone7@gmail.com>
2023-11-20 12:27:28 -0300

    ssdfsdfsdf

---
+++ src/app/reservar-cita/reserva/reserva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ReservarCitaPage } from './reservar-cita.page.ts';
+
+@Component({
+  selector: 'app-reserva',
+  templateUrl: './reserva.component.html',
+  styleUrls: ['./reserva.component.scss'],
+})
+export class ReservaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
